995681cdd105402a2c8939302ea9ac19abb36793
Kalpita Chaudhuri <kalpitachaudhuri01@gmail.com>
2023-03-10 23:54:24 +0530

    Customised the drawer

--- Tomlinson/CustomDrawer.js
+++ Tomlinson/CustomDrawer.js
@@ -1,13 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ImageBackground } from 'react-native'
 import React from 'react'
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
 
+
 const CustomDrawer = (props) => {
   return (
-    <DrawerContentScrollView {...props}> 
-    <DrawerItemList {...props}/> 
+    <View style={{flex:1}}>
+    <DrawerContentScrollView {...props} 
+    contentContainerStyle={{backgroundColor:'#CCCCFF'}}>
+     <ImageBackground 
+     source= {{uri : "https://pbs.twimg.com/media/FqbFwYoXoAUb9eV.jpg",}}
+     style={{ padding:20 }}>
+      <Image
+      source= {{uri: "https://w0.peakpx.com/wallpaper/581/788/HD-wallpaper-louis-tomlinson-blue-blue-indie-louis-tomlinson-tomlinson.jpg",}}
+      style={{
+        height: 80, 
+        width: 80, 
+        borderRadius: 50, 
+        marginBottom: 5,
+      }}
+      />
+      <Text style={{ 
+        fontSize: 20, 
+        fontWeight: "500",
+        color: 'white'}}>
+        Louis Tomlinson
+      </Text>
+     </ImageBackground>
+      <DrawerItemList {...props} /> 
     </DrawerContentScrollView>
+    </View>
   )
 }
 
-export default CustomDrawer
+export default CustomDrawer;
--- Tomlinson/Sidebar.js
+++ Tomlinson/Sidebar.js
@@ -6,15 +6,48 @@ import Career from './Career';
 import Tourdates from './Tourdates';
 import Achievement from './Achievement';
 import CustomDrawer from './CustomDrawer';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function Sidebar() {
    const Drawer = createDrawerNavigator();
     return (
     <Drawer.Navigator drawerContent={(props) => <CustomDrawer {...props} />} screenOptions={{ headerShown: true }}>
-        <Drawer.Screen name="Tommo" component={Tommo} />
-        <Drawer.Screen name="Career" component={Career} />
-        <Drawer.Screen name="Tourdates" component={Tourdates} />
-        <Drawer.Screen name="Achievements" component={Achievement} />
+        <Drawer.Screen 
+        name="Tommo" 
+        component={Tommo}
+        options={{
+          drawerIcon: ({ color }) => (
+            <FontAwesome name="crosshairs" size={20} color={color} />
+          ),
+        }}
+         />
+        <Drawer.Screen 
+        name="Career" 
+        component={Career}
+        options={{
+          drawerIcon: ({ color }) => (
+            <FontAwesome name="cubes" size={20} color={color} />
+          ),
+        }}
+        />
+        <Drawer.Screen 
+        name="Tourdates" 
+        component={Tourdates}
+        options={{
+          drawerIcon: ({ color }) => (
+            <FontAwesome name="calendar" size={20} color={color} />
+          ),
+        }}
+        />
+        <Drawer.Screen 
+        name="Achievements" 
+        component={Achievement}
+        options={{
+          drawerIcon: ({ color }) => (
+            <FontAwesome name="trophy" size={20} color={color} />
+          ),
+        }}
+        />
     </Drawer.Navigator>
   );
 }
